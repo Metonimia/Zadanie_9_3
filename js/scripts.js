4665e025ec4b48343e0139ab73b3779a184f84ee
Zuzanna <zuzanna.zuzewicz@gmail.com>
2017-11-22 21:01:54 +0200

    Added project structure

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+
+var dinosaurNew = dinosaur.toUpperCase();
+
+var textAfter = text.replace('Velociraptor', dinosaurNew);
+
+var textCutInHalf = textAfter.substr(0, textAfter.length/2);
+
+console.log(textCutInHalf);
+
